5387bef68b8ecd86d951365d424c41f1c0fbac59
jtammelleo <tammelleo@hotmail.com>
2023-01-06 19:03:46 -0500

    Remove click event pop-up element

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 
-document.querySelector("html").addEventListener("click", function () {
-  alert("Ouch! Stop poking me!");
-});
