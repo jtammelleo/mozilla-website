bff3245e2926a453bcf488ccc55d9e6b586547e1
jtammelleo <tammelleo@hotmail.com>
2023-01-06 19:06:05 -0500

    Remove Hello World js, add first attempt at logo click event

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
 
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "mozilla_logo.png") {
+    myImage.setAttribute("src", "mozilla_logo.png");
+  } else {
+    myImage.setAttribute("src", "mozilla_logo.png");
+  }
+};
